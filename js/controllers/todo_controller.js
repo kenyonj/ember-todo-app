5c39dfdb94578f27b85fc9b8c8d285e2d4e0b354
Justin Kenyon <kenyonj@gmail.com>
2014-03-24 21:21:18 -0400

    Add persistent data source

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,26 @@
 Todos.TodoController = Ember.ObjectController.extend({
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    },
+    removeTodo: function() {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    },
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    }
+  },
+
+  isEditing :false,
+
   isCompleted: function(key, value){
     var model = this.get('model');
 
